3310054803b50d87e2515550cf957e96a62026d9
shawon719 <sharmony.hashem2001@gmail.com>
2024-12-12 11:59:32 +0600

    add evidence file upload

---
+++ evidence17/fileupload.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>file and image upload</title>
+</head>
+<body>
+    <div class="formm">
+        <form action="#" method="post" enctype="multipart/form-data">
+            <input type="file" name="myfile">
+            <!-- <input type="submit" name="btn" val> -->
+             <button name="btn">upload</button>
+        </form>
+    </div>
+</body>
+</html>
