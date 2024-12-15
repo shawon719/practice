e60ff624c76d8a0f588021b4d2f0b073ee31f772
shawon719 <sharmony.hashem2001@gmail.com>
2024-12-15 09:16:16 +0600

    add evidence17

---
+++ evidence17/fileupload.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>file upload</title>
+</head>
+<body>
+     <section>
+        <form action="" method="post" enctype="multipart/form-data">
+                <h3>file upload</h3>
+                <input type="file" name="myFile">
+                <button name="btn">choose file</button>
+        </form>
+     </section>
+</body>
+</html>
