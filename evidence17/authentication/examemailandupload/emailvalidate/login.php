ecec6721b9704696777f891912f06a7a8ae1cc69
shawon719 <sharmony.hashem2001@gmail.com>
2024-12-15 12:31:02 +0600

    add

---
+++ evidence17/authentication/examemailandupload/emailvalidate/login.php
@@ -0,0 +1,54 @@
+<?php 
+    session_start();
+    if(isset($_POST["btn"])){
+         $username=$_POST["txtusername"];
+         $password=$_POST["txtpassword"];
+         $email=$_POST["txtemail"];
+         $a="/^[a-zA-Z0-9]+@+\.[a-zA-Z]{2,}$/";
+         if(preg_match($a,$email)){
+            echo "email valid.";
+         }else{
+            echo "email not valid.<br>";
+         }
+        if($username=="shawon" && $password=="123" && $email="$a"){
+            $_SESSION["sname"]=$username;
+             header("location:demo.php");
+        }
+        else{
+            $m="username,password  & email are incorrect!!!!!!!!!!";
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>log in page </title>
+</head>
+<body>
+
+        <?php 
+                echo isset($m)?$m:" ";
+        ?>
+    <form action="#" method="post">
+        <h1>this is first page</h1>
+        <table>
+            <tr>
+                <th><label for="">User Name:</label></th>
+                <th><input type="text" name="txtusername" id="" required></th> 
+            </tr>
+            <tr>
+                <th>Password:</th>
+                <th><input type="text" name="txtpassword" id="" required></th>
+            </tr>
+            <tr>
+                <th>Email:</th>
+                <th><input type="text" name="txtemail" required></th>
+            </tr>
+        </table>
+            <input type="submit" name="btn"  value="Log in" id="">
+    </form>
+</body>
+</html>
+
--- evidence17/fileupload.php
+++ evidence17/fileupload.php
@@ -1,16 +1,16 @@
 <?php
-    if(isset($_POST['btn'])){
-        $filename=$_FILES['myFile']['name'];
-        $temp_file=$_FILES['myFile']['tmp_name'];
-        $f_type=$_FILES['myFile']['type'];
-        $f_size=$_FILES['myFile']['size'];
+    // if(isset($_POST['btn'])){
+    //     $filename=$_FILES['myFile']['name'];
+    //     $temp_file=$_FILES['myFile']['tmp_name'];
+    //     $f_type=$_FILES['myFile']['type'];
+    //     $f_size=$_FILES['myFile']['size'];
         //var_dump($type);
         //echo "<br>";
         //var_dump($size);
         //echo "<br>";
        // var_dump($filename);
 
-        $img="image/";
+       // $img="image/";
         //  if(!empty($filename)){
         //     // move_uploaded_file ফাংশনটি ফাইলটি সঠিকভাবে নির্ধারিত ডিরেক্টরিতে মুভ করে
         //     if(move_uploaded_file($temp_file, $img . $filename)){
@@ -22,28 +22,45 @@
         // else{
         //     echo "please select a file";
         // }
-        $kb=$f_size/1024;
-        if($kb<100){
-            if(move_uploaded_file($temp_file, $img . $filename)){
+        ?>
+        
+<?php
+        // ফাইল ইনপুট থেকে ডেটা সংগ্রহ
+if (isset($_FILES['my_file'])) {
+    $f_size = $_FILES['my_file']['size'];  // ফাইল সাইজ
+    $f_type = strtolower(pathinfo($_FILES['my_file']['name'], PATHINFO_EXTENSION));  // ফাইল এক্সটেনশন
+    $temp_file = $_FILES['my_file']['tmp_name']; // ফাইলের টেম্পোরারি পাথ
+    $filename = $_FILES['my_file']['name'];  // আসল ফাইল নাম
+    $img = "image/";  // যেখানে ফাইল আপলোড হবে
+
+    // ফাইল সাইজ কিলোবাইটে রূপান্তর
+    $kb = $f_size / 1024;
+
+    // ফাইল সাইজ চেক
+    if ($kb < 100) {
+        if (move_uploaded_file($temp_file, $img . $filename)) {
             echo "file size is okay and upload successful";
-            }else{
-                echo "file size is bigger.";
-            }
+        } else {
+            echo "there is a problem uploading the file";
         }
-        else if($f_type=='png'){
-            if(move_uploaded_file($temp_file, $img . $filename)){
-            echo "file type match and upload successful";
-            }else{
-                echo "not a png file.";
-            }
-        }
-        else{
-            echo "please choose file.";
-        }
-
-
     }
+    // PNG ফাইল চেক
+    else if ($f_type == 'jpg') {
+        if (move_uploaded_file($temp_file, $img . $filename)) {
+            echo "file type match and upload successful";
+        } else {
+            echo "not a png file.";
+        }
+    } else {
+        echo "file size is bigger than 100KB";
+    }
+
+} 
+else {
+    echo "please choose file.";
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -55,16 +72,16 @@
      <section>
         <form action="" method="post" enctype="multipart/form-data">
                 <h3>file upload</h3>
-                <input type="file" name="myFile">
+                <input type="file" name="my_file">
                 <button name="btn">choose file</button>
         </form>
      </section>
      <?php
-          // ফাইল আপলোড সফল হলে ইমেজ দেখানোর জন্য
-         //if(isset($_POST["btn"]) && (!empty($filename))){
-        //     // ইমেজটি সঠিকভাবে প্রদর্শন করার জন্য সঠিক পাথ ব্যবহার
-           // echo "<img src='$img$filename' alt='uploaded image' width='300px'>";
-         //}
+          //ফাইল আপলোড সফল হলে ইমেজ দেখানোর জন্য
+         if(isset($_POST["btn"]) && (!empty($filename))){
+            // ইমেজটি সঠিকভাবে প্রদর্শন করার জন্য সঠিক পাথ ব্যবহার
+           echo "<img src='$img$filename' alt='uploaded image' width='300px'>";
+         }
      ?> 
 </body>
 </html>
