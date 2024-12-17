a9ae79e723c230a100acea6f7571ea67e7755b89
shawon719 <sharmony.hashem2001@gmail.com>
2024-12-17 11:54:47 +0600

    add 17 evidence

---
+++ 17evidence/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login page </title>
+</head>
+<body>
+    <section class="fo">
+        <form action="" method="post">
+            <div>
+                Username:<input type="text" name="uname">
+            </div><br>
+            <div>
+                Password:<input type="password" name="pname">
+            </div><br>
+            <div>
+                Email:<input type="text" name="ename">
+            </div>
+            <div>
+                <button name="logbtn">Log In</button>
+                <button>Sign In</button>
+                
+            </div>
+        </form>
+    </section>
+</body>
+</html>
