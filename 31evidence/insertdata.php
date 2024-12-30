a0d6452333fc3d836ccd48e792d8b8b2e2be954a
shawon719 <sharmony.hashem2001@gmail.com>
2024-12-30 11:05:26 +0600

    add product table

--- 31evidence/insertdata.php
+++ 31evidence/insertdata.php
@@ -6,9 +6,19 @@
             $address=$_POST["adName"];
             $contact=$_POST["coName"];
             $db->query("call manuf('$mName','$address','$contact')");
-            echo "added.";
+            //echo "added.";
         }else{
-            echo "not added;";
+            //echo "not added;";
+        }
+
+        if(isset($_POST["proSub"])){
+            $pname=$_POST["ame"];
+            $price=$_POST["pp"];
+            $mid=$_POST["manufact"];
+            $db->query("call prod('$pname','$price','$mid')");
+           // echo "product added;";
+        }else{
+            //echo "not added.";
         }
 ?>
 
@@ -47,10 +57,61 @@
                         
                     </tr>
                 </tfoot>
-                 
+                <?php
+                    if(isset($_POST["maSub"])){
+                                echo "added.";
+                    }else{
+                        echo "not added.";
+                    }        
+                ?>
             </table>
         </form>
     </section> 
+
+    <section>
+        <h1>this is product table.</h1>
+            <form action="" method="post">
+            <table border="1" style="border-collapse:collapse">
+                <thead>
+                    <tr>
+                        <th>Product Name</th>
+                        <th>Price<br></th>
+                        <th>Manufacture Name<br></th>
+                    </tr>
+                   
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><input type="text" name="ame"></td>
+                        <td><input type="text" name="pp" id=""></td>
+                        <td>
+                            <select name="manufact" id="">
+                                <?php
+                                    $manuName=$db->query("select * from manufacturer");
+                                     while(list($_mid,$_mNm)=$manuName->fetch_row()){
+                                        echo "<option value='$_mid'>$_mNm</option>";
+                                    }       
+                                ?>
+                            </select>
+                        </td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan="3" style="text-align:center"><button name="proSub">Add into Manufacture</button></td>
+                        
+                    </tr>
+                </tfoot>
+                <?php
+                    if(isset($_POST["proSub"])){
+                                echo "added.";
+                    }else{
+                        echo "not added.";
+                    }  
+                ?>     
+            </table>
+        </form>
+    </section>
 
    
 </body>
